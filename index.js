7a8f667e16f30fd2af8f46a2fb61dca2b0703733
Dinh Lap <63952696+kogs11@users.noreply.github.com>
2020-07-16 20:16:48 +0700

    lap-update

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ const fs = require("fs");
 app.get("/khuyenmai",function(req,res){
     res.render("nam");
 });
+
+app.get("/menu", function(req,res) {
+    let data = fs.readFileSync("data/sanphamburger.json","utf-8");
+    let burgers = JSON.parse(data);
+    let productId = req.params.id;
+    let product = {};
+    burgers.map(function (e) {
+        if(e.id == productId){
+            product = e;
+        };
+    })
+    res.render("lap_shop",{
+        burgers: burgers,
+        product: product
+    });
+});
