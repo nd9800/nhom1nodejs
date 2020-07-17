a2c43423b54f0cd7ee65ae4018987e056f32d067
NamD <64962991+nd9800@users.noreply.github.com>
2020-07-18 02:30:53 +0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
+
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,function () {
-    console.log("sever is running...");
+    console.log("server is running...");
 });
 app.use(express.static("public"));
 app.set("view engine","ejs");
 const fs = require("fs");
-app.get("/khuyenmai",function(req,res){
-    res.render("nam");
-});
+
 
 app.get("/menu", function(req,res) {
     let data = fs.readFileSync("data/sanphamburger.json","utf-8");
@@ -31,10 +30,13 @@ app.get("/home",function(req,res){
 });
 app.get("/khuyen-mai",function(req,res){
     res.render("khuyen-mai");
+
 });
+
 app.get("/khuyen-mai/mua-1-tang-1-thu-nam",function(req,res){
     res.render("mua-1-tang-1-thu-nam");
 });
+
 app.get("/khuyen-mai/combo-de-khang",function(req,res){
     res.render("combo-de-khang");
 });
